7c04e5cd58732bd2a89a08700662df108413a956
Pete Corey <petecorey@gmail.com>
2016-07-05 09:09:17 -0400

    # The Simplest Test
    The first thing we do when we're solving a problem like this is to write
    a test. Because we're implementing a function given to us, we already
    know what our interface will look like (`pointInPoly`), so we can
    immediately write a test for it.
    
    Our first test asserts that a point at the origin (`[0, 0]`) is within a
    simple triangle with points at `[-1, -1]`, `[1, -1]`, and `[0, 1]`.
    
    After writing this test, our test suite complains that `pointInPoly is
    not defined`. This is quickly remediated by importing `pointInPoly` into
    our test file and then exporting it from `index.js`.
    
    After exporting the empty `pointInPoly` function, the test suite shouts
    that it `expected undefined to be true`. A quick fix to bring us back to
    green is to simply have our new `pointInPoly` function return `true`.

---
+++ index.js
@@ -0,0 +1,3 @@
+export function pointInPoly(poly, point) {
+    return true;
+}
--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
 import { expect } from "chai";
+import { pointInPoly } from "../";
 
-describe("index", function() {
+describe("pointInPoly", function() {
 
-    it("works", function() {
-        expect(2+2).to.equal(4);
+    it("detects a point in a triangle", function() {
+        let poly = [[-1, -1], [1, -1], [0, 1]];
+        expect(pointInPoly(poly, [0, 0])).to.be.true;
     });
 
 });
