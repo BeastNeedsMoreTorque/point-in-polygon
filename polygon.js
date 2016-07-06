926942cc46739153084f9eaee0ac5716c448da3b
Pete Corey <petecorey@gmail.com>
2016-07-06 09:16:26 -0400

    # Restating Point-in-Polygon
    Now that we've defined our `polygon`, we can restate our high-level
    point-in-polygon test. We want to `translate` our polygon so that the
    point we've been given acts like a ray from the origin. Next we want to
    count the number of intersections that ray makes with the newly
    translated polygon:
    
        let intersections = translate([-x, -y]).intersections([0, 1]);
    
    Lastly we want to return `true` from `surrounds` if `intersections` is
    odd:
    
        return !!(intersections % 2);
    
    After making these changes, our test suite complains about `translate`
    and `intersections` not being defined.
    
    The fastest way to get us back to green is to have `translate` return a
    new `polygon`, and have `intersections` return `1`.

--- polygon.js
+++ polygon.js
@@ -1,8 +1,19 @@
 export function polygon(_points) {
 
-    let surrounds = (point) => true;
+    let surrounds = ([x, y]) => {
+        let intersections = translate([-x, -y]).intersections([0, 1]);
+        return !!(intersections % 2);
+    };
+
+    let translate = ([x, y]) => {
+        return polygon(_points);
+    };
+
+    let intersections = (ray) => 1;
 
     return {
-        surrounds
+        surrounds,
+        translate,
+        intersections
     };
 }
